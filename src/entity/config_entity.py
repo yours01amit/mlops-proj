9d1e4694bfcce4dc8ad8618d05b0bd7bd14f89b7
Yours01amit <emmysingh019@gmail.com>
2025-02-04 00:34:36 +0530

    fastapi app is ready

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -63,7 +63,7 @@ class ModelPusherConfig:
     bucket_name: str = MODEL_BUCKET_NAME
     s3_model_key_path: str = MODEL_FILE_NAME
 
-# @dataclass
-# class VehiclePredictorConfig:
-#     model_file_path: str = MODEL_FILE_NAME
-#     model_bucket_name: str = MODEL_BUCKET_NAME
+@dataclass
+class VehiclePredictorConfig:
+    model_file_path: str = MODEL_FILE_NAME
+    model_bucket_name: str = MODEL_BUCKET_NAME
